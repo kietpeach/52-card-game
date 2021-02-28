e228caf185700c2fba79e61ab9113f1f6c84ca43
Kiet Peach <daovankiet93@gmail.com>
2021-02-28 16:13:46 +0700

    cm

--- Lobby.cs
+++ Lobby.cs
@@ -3,27 +3,43 @@ using System;
 
 public class Lobby : Control
 {
-    //Button Start;
+    Button Join;
+    VBoxContainer LobbyList;
 
     public override void _Ready()
     {
-        //Start = GetNode("StartButton") as Button;
+        Join = GetNode("JoinButton") as Button;
+        LobbyList = GetChild(6).GetChild(0).GetChild(3).GetNode("LobbyList") as VBoxContainer;
+
     }
-    private void _on_StartButton_pressed()
+    private void JoinGame()
     {
         GetTree().ChangeScene("res://Game.tscn");
     }
-    private void _on_JointButton_pressed()
+    private void _on_JoinButton_pressed()
     {
         GD.Print("popup");
         Popup popup = GetNode<Popup>("PopupPanel");
         popup.Popup_();
+        //
+        var LobbyName = "Kiet";
+        var LobbyMember = 4;
+        var LobbyButton = new Button();
+        LobbyButton.Text = "LobbyName:"+LobbyName+"     "+"Member:"+LobbyMember;
+        LobbyButton.Connect("pressed",this,"JoinGame");
+        LobbyList.AddChild(LobbyButton);
     }
+
     private void _on_CloseButton_pressed()
     {
         GD.Print("hide");
         Popup popup = GetNode<Popup>("PopupPanel");
         popup.Hide();
+    }
+    private void _on_Refresh_pressed()
+    {
+        GD.Print("Refresh lobby list");
+        //TODO
     }
 
 
